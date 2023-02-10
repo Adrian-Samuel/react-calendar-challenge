3f7dad6534afe44e6684c139295bb93ec3508d0c
Adrian Samuel <adrian.samuel@machinemax.com>
2023-02-10 13:58:46 +0000

    added form to the body of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import CalendarBody from "./components/Calendar/CalendarBody";
+import CalendarForm from './components/Calendar/Form'
 import "./App.css";
 
 function App() {
+
+  // state should be declared at the parent level,
+  // could use useContext with a reducer but there's little point
+
   return (
     <div className="App">
       <CalendarBody />
+      <CalendarForm />
     </div>
   );
 }
