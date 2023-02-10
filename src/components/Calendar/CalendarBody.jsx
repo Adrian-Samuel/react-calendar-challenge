3b846380fde720339a11ff4ec160bc16a99de5bc
Adrian Samuel <adrian.samuel@machinemax.com>
2023-02-10 12:57:02 +0000

    add calendar body and fill it with row and header component

---
+++ src/components/Calendar/CalendarBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TableHeaders from "./Header";
+import TableRow from "./Row";
+
+export default () => {
+  return (
+    <table>
+      <TableHeaders />
+      <TableRow />
+    </table>
+  );
+};
