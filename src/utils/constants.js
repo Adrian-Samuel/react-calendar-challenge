fc57e3aff6847b1862152c7892941144ee1625ea
Adrian Samuel <adrian.samuel@machinemax.com>
2023-02-13 06:17:04 +0000

    moved calendar generator back to utils

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,3 @@
-const SHIFT_LENGTH = 8;
-const startHour = new Date(new Date().setHours(9, 0, 0, 0)).getHours();
-
-export const hoursInDay = [
-  startHour,
-  ...Array.from({ length: SHIFT_LENGTH }, (calendarTime, index) => {
-    const hourDiff = index + 1;
-    return new Date(
-      new Date().setHours(startHour + hourDiff, 0, 0, 0)
-    ).getHours();
-  }),
-];
-
 export const daysOfWeek = [
   "Monday",
   "Tuesday",
@@ -18,3 +5,42 @@ export const daysOfWeek = [
   "Thursday",
   "Friday",
 ];
+export const SHIFT_LENGTH = 8;
+export const startHour = new Date(new Date().setHours(9, 0, 0, 0)).getHours();
+
+export const formatDateTime = (hour) => `${hour}:00`;
+
+export const calendarState = (
+  startHour,
+  shiftLength,
+  timeInterval,
+  weekDays,
+  timeFormatter
+) => {
+  const times = [
+    startHour,
+    ...Array.from({ length: shiftLength }, (calendarTime, index) => {
+      const hourDiff = index + timeInterval;
+      return new Date(
+        new Date().setHours(startHour + hourDiff, 0, 0, 0)
+      ).getHours();
+    }),
+  ].map((hour) => ({
+    hour: timeFormatter(hour),
+    eventTimes: [],
+  }));
+
+  const dayMap = weekDays.reduce((dayMap, day) => {
+    dayMap[day] = {};
+    return dayMap;
+  }, {});
+
+  return Object.entries(dayMap).reduce((days, [key]) => {
+    times.forEach((time) =>
+      days.hasOwnProperty(key) > 0
+        ? (days[key] = { ...days[key], [time.hour]: time.eventTimes })
+        : (days[key] = { [time.hour]: time.eventTimes })
+    );
+    return days;
+  }, dayMap);
+};
